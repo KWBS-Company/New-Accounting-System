c433dd118f4f1b2aec22712231f8f9305286ed61
Aanniiz123 <anishpudasaini25@gmail.com>
2026-07-02 18:11:10 +0545

    Implementation of Ai-chatbot

--- ai-backend/src/app.py
+++ ai-backend/src/app.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
+
 from src.api.health import router as health_router
+from src.api.model import router as model_router
+from src.api.chat import router as chat_router
 
 
 def create_app():
-    
     app = FastAPI(
-        description="The Accounting API description",
-        title="AI Backend"
-        )
+        title="AI Backend",
+        description="The AI Backend API",
+    )
 
+    # Health APIs
     app.include_router(health_router)
+
+    # Model APIs
+    app.include_router(model_router)
+
+    # Chat api
+    app.include_router(chat_router)
 
     return app
